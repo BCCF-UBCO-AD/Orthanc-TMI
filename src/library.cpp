3c339eef1953207ed0004968fc4e2b9e76f00dec
Josh Cooper <cooper.s.josh@outlook.com>
2021-10-02 21:09:53 -0700

    Removes library.cpp in favor of plugin.cpp

--- src/library.cpp
+++
@@ -1,9 +0,0 @@
-int32_t OrthancPluginInitialize(OrthancPluginContext* context){
-}
-void OrthancPluginFinalize(){
-}
-const char* OrthancPluginGetName(){
-}
-const char* OrthancPluginGetVersion(){
-}
-
