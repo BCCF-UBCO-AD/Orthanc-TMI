efd5220069e836641c5bddcfed9bd8c0dd82d034
Josh Cooper <cooper.s.josh@outlook.com>
2021-10-07 11:21:07 -0700

    Deletes run-docker.sh

--- run-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wd=$(realpath $(dirname "${BASH_SOURCE[0]}"))
-mkdir $wd/docker/orthanc-db
-mkdir $wd/docker/plugins
-sudo docker run -p 4242:4242 -p 8042:8042 --rm -v $wd/docker/orthanc.json:/etc/orthanc/orthanc.json:ro -v $wd/docker/orthanc-db/:/var/lib/orthanc/db/ -v $wd/docker/plugins:/usr/share/orthanc/plugins jodogne/orthanc-plugins
