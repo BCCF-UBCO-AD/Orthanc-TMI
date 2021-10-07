a7d842075e1af683220c92de3111b6aa3e57c9e0
Josh Cooper <cooper.s.josh@outlook.com>
2021-10-06 22:38:18 -0700

    Fixes unit test errors

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,7 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+//appears to be the form of a basic unit test
+// https://github.com/google/googletest/blob/master/googletest/samples/sample1_unittest.cc
+// https://google.github.io/googletest/primer.html
+TEST(TestSuiteName, TestName) {
 }
