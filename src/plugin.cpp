c9b2e4ae435174c150d53475d8ef1681a33edbf4
Josh Cooper <cooper.s.josh@outlook.com>
2021-10-22 14:30:15 -0700

    Adds and registers filter callback and adds Anonymize function

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,12 +1,18 @@
 #include <orthanc/OrthancCPlugin.h>
 #include "configuration.h"
 
-static OrthancPluginContext* context_ = nullptr;
+namespace globals {
+    static OrthancPluginContext *context = nullptr;
+}
 
+// prototypes
+int32_t FilterCallback(const OrthancPluginDicomInstance* instance);
+OrthancPluginDicomInstance* Anonymize(const OrthancPluginDicomInstance* instance);
+
+// plugin foundation
 extern "C" {
     int32_t OrthancPluginInitialize(OrthancPluginContext* context){
-        context_ = context;
-
+        globals::context = context;
         /* Check the version of the Orthanc core */
         if (OrthancPluginCheckVersion(context) == 0){
             char info[256];
@@ -19,6 +25,8 @@ extern "C" {
             OrthancPluginLogError(context, info);
             return -1;
         }
+        OrthancPluginRegisterIncomingDicomInstanceFilter(context, FilterCallback);
+
         return 0;
     }
     
@@ -33,4 +41,31 @@ extern "C" {
     const char* OrthancPluginGetVersion(){
         return ORTHANC_PLUGIN_VERSION;
     }
+}
+
+int32_t FilterCallback(const OrthancPluginDicomInstance* instance){
+    // todo: possibly copy instance data to new buffer to control life span, then anonymize as a job instead of in this callstack
+    auto new_instance = Anonymize(instance);
+    return 0; /*{0: discard, 1: store, -1: error}*/
+}
+
+//#include <cstdlib>
+OrthancPluginDicomInstance* Anonymize(const OrthancPluginDicomInstance* instance){
+    using namespace globals;
+    const void* data = OrthancPluginGetInstanceData(context, instance);
+    int64_t size = OrthancPluginGetInstanceSize(context, instance);
+    const char* readable_buffer = (const char*)data;
+    //    for(int64_t i = 0; i < size; ++i){
+    //        printf("%c", readable_buffer[i]);
+    //    }
+
+    // extract header
+    // extract PHI
+    // extract images
+    // compile new dicom file {header, image}
+    // perform PHI lookup
+    // if not found, insert PHI
+    // if potential match found link records
+
+    return nullptr;
 }
