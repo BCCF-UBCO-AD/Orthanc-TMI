2decfa57e09096f99337a8fa4de039f9106444b1
Josh Cooper <cooper.s.josh@outlook.com>
2021-10-30 15:11:46 -0700

    Adds PopulateFilterList() and integrates DicomFilter

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,16 +1,21 @@
-#include <orthanc/OrthancCPlugin.h>
-#include <nlohmann/json.hpp>
+#define IMPLEMENTS_GLOBALS
 #include "configuration.h"
+#include "core.h"
+#include "dicom-filter.h"
 
-#include <string>
+#include <nlohmann/json.hpp>
 
+namespace nlm = nlohmann;
 namespace globals {
-    static OrthancPluginContext *context = nullptr;
+    OrthancPluginContext *context = nullptr;
+    std::unordered_set<uint32_t> filter_list;
 }
 
 // prototypes
 int32_t FilterCallback(const OrthancPluginDicomInstance* instance);
 const OrthancPluginDicomInstance* Anonymize(const OrthancPluginDicomInstance* readonly_instance);
+void PopulateFilterList();
+const char* ParseTag(const char* buffer, nlm::json config);
 
 // plugin foundation
 extern "C" {
@@ -28,6 +33,7 @@ extern "C" {
             OrthancPluginLogError(context, info);
             return -1;
         }
+        PopulateFilterList();
         OrthancPluginRegisterIncomingDicomInstanceFilter(context, FilterCallback);
 
         return 0;
@@ -48,6 +54,14 @@ extern "C" {
 
 using namespace globals;
 
+void PopulateFilterList(){
+    nlm::json config(OrthancPluginGetConfiguration(context));
+    for(uint32_t tag : config["filtered-tags"]){
+        // todo: implement me, tags will be stored as strings they need to be converted (probably)
+        filter_list.emplace(tag);
+    }
+}
+
 int32_t FilterCallback(const OrthancPluginDicomInstance* instance){
     // todo: possibly copy instance data to new buffer to control life span, then anonymize as a job instead of in this callstack
     auto new_instance = Anonymize(instance);
@@ -59,40 +73,8 @@ int32_t FilterCallback(const OrthancPluginDicomInstance* instance){
     }
     return 0; /*{0: discard, 1: store, -1: error}*/
 }
-//#include <cstdlib>
+
 const OrthancPluginDicomInstance* Anonymize(const OrthancPluginDicomInstance* readonly_instance){
-    const void* data = OrthancPluginGetInstanceData(context, readonly_instance);
-    int64_t size = OrthancPluginGetInstanceSize(context, readonly_instance);
-    const char* readable_buffer = (const char*)data;
-    size_t preamble = 128;
-    size_t prefix = 4;
-    if(size <= preamble + prefix){
-        // throw error, or something
-    }
-    if(std::string_view(readable_buffer+preamble,prefix) != "DICM"){
-        // apparently not a DICOM file... so...
-        // throw error, or something
-    }
-    // move the read head to where the tag data begins
-    readable_buffer = readable_buffer+preamble+prefix;
-    nlohmann::basic_json config(OrthancPluginGetConfiguration(context));
-    // todo: define config format expectations
-    // read config, store group+element strings: data-filter
-    // parse data, check data-filter for each new tag
-
-    // todo: read file data, tag by tag, printing each tag and its various fields on new lines
-    //    for(int64_t i = 0; i < size; ++i){
-    //        printf("%s \n", tag_line);
-    //    }
-    bool no_removable_data = false;
-
-    // compile new dicom file {header, image}
-    // perform PHI lookup
-    // if not found, insert PHI
-    // if potential match found link records
-    if(no_removable_data){
-        return readonly_instance;
-    }
-
-    return nullptr;
+    DicomFilter parser(readonly_instance);
+    return parser.GetFilteredInstance();
 }
