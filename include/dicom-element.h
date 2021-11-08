fe4d329085eb3d1ca6186253f7cd219bb0cb852a
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-08 13:02:35 -0800

    Adds preprocessor enabled debugging to dicom-element.h

--- include/dicom-element.h
+++ include/dicom-element.h
@@ -1,8 +1,7 @@
 #pragma once
-
 #include <string>
 #include "core.h"
-extern std::string DecToHex(uint16_t value);
+extern std::string DecToHex(uint64_t value);
 extern uint32_t HexToDec(std::string hex);
 
 class DicomElement {
@@ -13,16 +12,32 @@ public:
     const uint16_t &group = *(uint16_t *)(buffer + idx);
     const uint16_t &element = *(uint16_t *)(buffer + idx + 2);;
     const std::string VR = std::string(std::string_view(buffer + idx + 4, 2));
-    const uint32_t length;
-    const uint64_t size;
-    const std::string byte1;
+    const uint32_t length = CalcLength();
+    const uint64_t size = CalcSize();
 
-    DicomElement(const char* data, uint64_t index)
-            : buffer(data),
-              idx(index),
-              length(CalcLength(data, index, VR)),
-              size(CalcSize(data, index, length)){}
-    std::string HexTag() const { return DecToHex(group).append("," + DecToHex(element));}
+    DicomElement(const char* buffer, uint64_t index, const char* hex_buffer = nullptr) : buffer(buffer), idx(index){
+#ifdef DEBUG_
+        std::string value;
+        if(VR == "UL") {
+            value = std::to_string(*(uint32_t*)(buffer+idx+bytes));
+        } else if (hex_buffer) {
+            value = std::string((std::string_view(hex_buffer+(2*idx)+(2*bytes),2*length)));
+        }
+
+        printf("(%s)->(%s,%s) [%s] - value length: %d - (header: %zu Bytes; size: %zu Bytes)(index=%zu->%zu)\n",
+               HexTag().c_str(),
+               HexGroup().c_str(),
+               HexElement().c_str(),
+               VR.c_str(),
+               length,
+               bytes,
+               size,
+               idx,
+               GetNextIndex());
+        printf("  %s\n", value.c_str());
+#endif
+    }
+    std::string HexTag() const { return DecToHex(tag);}
     std::string HexGroup() const { return DecToHex(group); }
     std::string HexElement() const { return DecToHex(element); }
     uint64_t GetNextIndex() const { return idx + size; }
@@ -30,22 +45,35 @@ protected:
     bool require_length = false;
     bool has_reserved = false;
     size_t bytes = 0;
-    uint32_t CalcLength(const char *data, uint64_t idx, std::string VR) {
+    uint32_t CalcLength() {
         require_length = VR == "UT";
         has_reserved = require_length || VR == "OB" || VR == "SQ" || VR == "OW" || VR == "UN";
         uint32_t value;
         if (has_reserved) {
             bytes = 12;
-            value = *(uint32_t *) (data + idx + 8); // 4 bytes
+            value = *(uint32_t *) (buffer + idx + 8); // 4 bytes
         } else {
             bytes = 8;
-            value = *(uint16_t *) (data + idx + 6); // 4 bytes
+            value = *(uint16_t *) (buffer + idx + 6); // 4 bytes
         }
-        return value == -1 ? 0 : value;
+        return value;
     }
 
-    uint64_t CalcSize(const char* data, uint64_t idx, uint32_t length) {
+    uint64_t CalcSize() {
         //runs after CalcLength
+        if(length == -1){
+#ifdef DEBUG_
+            printf("calc size: invalid length\n");
+#endif
+            //todo: search for delimiter
+            DicomElement element(buffer,idx+bytes);
+            size_t bytes = element.size - element.length;
+#ifdef DEBUG_
+            printf("calc size: %zu", bytes + element.size);
+            printf("(%s)=>%d [%s] - value length: %d - (header: %zu Bytes; size: %zu)(index=%zu->%zu)\n", element.HexTag().c_str(), element.tag, element.VR.c_str(), element.length, bytes, element.size, element.idx, element.GetNextIndex());
+#endif
+            return length + element.size;
+        }
         return length + bytes;
     }
 };
--- src/dicom-element.cpp
+++ src/dicom-element.cpp
@@ -1,10 +1,14 @@
 #include <dicom-element.h>
 #include <sstream>
 
-std::string DecToHex(uint16_t value) {
+std::string DecToHex(uint64_t value) {
     std::stringstream stream;
     stream << std::hex << value;
-    return std::string(stream.str());
+    std::string ret = std::string(stream.str());
+    if(ret.size() & 1){
+        ret.insert(0,"0");
+    }
+    return ret;
 }
 uint32_t HexToDec(std::string hex) {
     uint32_t x;
