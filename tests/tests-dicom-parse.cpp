1efbb967c9b08638c69791fe36eb204a1ab100bc
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-08 16:35:48 -0800

    Updates dicom-parser-test

--- tests/tests-dicom-parse.cpp
+++ tests/tests-dicom-parse.cpp
@@ -11,15 +11,29 @@ extern void parse_dicom(const char* hex_buffer, const char* buffer, size_t size)
 extern uint32_t read_tag_code(std::string hex);
 
 TEST(dicom_parsing, dicom_parse) {
-    fs::path rel_dicom("../samples/0002.DCM");
+    fs::path rel_dicom("../../samples/0002.DCM");
     std::ostringstream buf;
     std::ifstream file(rel_dicom);
     buf << file.rdbuf();
     std::string bufstr(buf.str());
+    size_t size = bufstr.size();
     char* hex_buffer = bytebuf_to_hexbuf(bufstr);
-    char* buffer = new char[bufstr.size()];
+    char* buffer = new char[size];
     memcpy(buffer,bufstr.c_str(),bufstr.size());
-    parse_dicom(hex_buffer, buffer, bufstr.size());
+    char msg_buffer[256] = {0};
+    size_t preamble = 128;
+    size_t prefix = 4;
+    printf("check size\n");
+    ASSERT_TRUE(size > preamble + prefix);
+    ASSERT_TRUE(std::string_view(buffer+preamble,prefix) == "DICM");
+    // move the read head to where the tag data begins
+    //buffer = buffer+preamble+prefix;
+    printf("read tags\n");
+    for(size_t i = preamble+prefix; i < size;){
+        DicomElement element(buffer, i, hex_buffer);
+        //printf("parse_dicom: bytes: %zu (%zu)\n", element.bytes, element.idx);
+        i = element.GetNextIndex();
+    }
     delete[] hex_buffer;
     delete[] buffer;
 }
