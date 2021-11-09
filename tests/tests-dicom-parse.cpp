492a4ba28288ce839b71c405c32ff80eeece0e65
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-08 17:02:18 -0800

    Adds path fixing code for dicom_parse unit test

--- tests/tests-dicom-parse.cpp
+++ tests/tests-dicom-parse.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#undef DEBUG_
 #include <dicom-element.h>
 #include <filesystem>
 #include <fstream>
@@ -11,10 +12,21 @@ extern void parse_dicom(const char* hex_buffer, const char* buffer, size_t size)
 extern uint32_t read_tag_code(std::string hex);
 
 TEST(dicom_parsing, dicom_parse) {
-    fs::path rel_dicom("./samples/0002.DCM");
+    fs::path exec_path(fs::canonical("/proc/self/exe"));
+    fs::path dicom;
+    if(exec_path.empty()) {
+        dicom = fs::path("./samples/0002.DCM");
+    } else {
+        exec_path = exec_path.parent_path();
+        if(exec_path.string().find("build/bin") != std::string::npos){
+            exec_path = fs::canonical(exec_path.string() + "/../../");
+        }
+        dicom = fs::path(exec_path.string() + "/samples/0002.DCM");
+    }
+    std::cout << dicom;
     std::ostringstream buf;
-    std::ifstream file(rel_dicom);
-    ASSERT_TRUE(file.is_open());
+    std::ifstream file(dicom);
+    //ASSERT_TRUE(file.is_open());
     buf << file.rdbuf();
     std::string bufstr(buf.str());
     size_t size = bufstr.size();
@@ -24,17 +36,17 @@ TEST(dicom_parsing, dicom_parse) {
     char msg_buffer[256] = {0};
     size_t preamble = 128;
     size_t prefix = 4;
-    printf("check size\n");
     ASSERT_TRUE(size > preamble + prefix);
     ASSERT_TRUE(std::string_view(buffer+preamble,prefix) == "DICM");
     // move the read head to where the tag data begins
     //buffer = buffer+preamble+prefix;
-    printf("read tags\n");
-    for(size_t i = preamble+prefix; i < size;){
+    size_t i = 0;
+    for(i = preamble+prefix; i < size;){
         DicomElement element(buffer, i, hex_buffer);
         //printf("parse_dicom: bytes: %zu (%zu)\n", element.bytes, element.idx);
         i = element.GetNextIndex();
     }
+    ASSERT_TRUE(i == size);
     delete[] hex_buffer;
     delete[] buffer;
 }
