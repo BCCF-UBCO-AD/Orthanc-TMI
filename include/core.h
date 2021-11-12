c98d617fc75193019259d78abb713bdc079d01d6
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-12 11:01:29 -0800

    Adds alias TagFilter

--- include/core.h
+++ include/core.h
@@ -2,9 +2,11 @@
 #include <orthanc/OrthancCPlugin.h>
 #include <unordered_set>
 
+using TagFilter = std::unordered_set<uint32_t>;
+
 #ifndef IMPLEMENTS_GLOBALS
 namespace globals {
     extern OrthancPluginContext* context;
-    extern std::unordered_set<uint32_t> filter_list;
+    extern TagFilter filter_list;
 }
 #endif
