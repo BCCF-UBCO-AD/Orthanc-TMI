e80bdccbdf73912ed7d77d300850cfc8cec7c6ed
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-17 17:32:31 -0800

    Finishes refactor of pqxx::connection instance to a pointer

--- include/db-interface.h
+++ include/db-interface.h
@@ -4,11 +4,9 @@
 
 // todo: name better?
 class DBInterface { // not an 'interface'
-private:
-    static pqxx::connection con;
 public:
     static void connect(std::string password);
     static void disconnect();
-    static bool is_open() { return con.is_open(); }
+    static bool is_open();
     static void HandlePHI(const DicomFile &dicom);
 };
--- src/db-interface.cpp
+++ src/db-interface.cpp
@@ -1,20 +1,25 @@
 #include "db-interface.h"
 #include "pqxx/except.hxx"
 
-pqxx::connection DBInterface::con;
+pqxx::connection* con = nullptr;
 
 void DBInterface::connect(std::string password) {
-    if(!con.is_open()) {
+    if(!con) {
         char buffer[256];
         sprintf(buffer, "postgresql://postgres:%s@localhost:5432", password.c_str());
-        con = pqxx::connection(buffer);
+        static pqxx::connection c(buffer);
+        con = &c;
     }
 }
 
 void DBInterface::disconnect() {
-    if(con.is_open()){
-        con.close();
+    if(con && con->is_open()){
+        con->close();
     }
+}
+
+bool DBInterface::is_open() {
+    return con && con->is_open();
 }
 
 void DBInterface::HandlePHI(const DicomFile &dicom) {
