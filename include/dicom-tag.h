b25b4042f6b193c6953e2ec3e41b71851b438620
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-20 18:45:42 -0800

    Adds GROUP_MASK definition to dicom-tag.h

--- include/dicom-tag.h
+++ include/dicom-tag.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <string>
+#define GROUP_MASK ((1<<16)-1)
+
 // convert decimal to hex, and pad the string to size `2*bytes`
 extern std::string DecToHex(uint64_t value, uint8_t bytes = 1);
+
 // convert up to 8 bytes of hex to decimal
 extern uint64_t HexToDec(std::string hex);
 
