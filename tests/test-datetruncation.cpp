2a2d0cd1cc3445b0af98bb4101ffb6dee62a04a4
Iwan Levin <apple.iwan@gmail.com>
2021-11-26 13:38:42 -0800

    added absolute path to json config

--- tests/test-datetruncation.cpp
+++ tests/test-datetruncation.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 #include <date-truncation.h>
+#include <fstream>
 #include "core.h"
 #include "plugin-configure.h"
 
-nlm::json config;
+static nlm::json config;
+static std::ifstream file("../../docker/orthanc/orthanc.json");
 
 TEST(datetrunc, trunc_test){
-    config = nlm::json::parse(OrthancPluginGetConfiguration(globals::context));
+    file >> config;
     std::string value = DateTruncation(config, "19990323");
     ASSERT_STREQ(value.c_str(), "19990323");
 }
