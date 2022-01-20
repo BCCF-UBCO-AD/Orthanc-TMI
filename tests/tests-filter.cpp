3efdcde87b5257a5221f9e8d305b342d1240e1bf
Josh Cooper <cooper.s.josh@outlook.com>
2022-01-20 14:59:07 -0800

    Improves filter unit test readability and output

--- tests/tests-filter.cpp
+++ tests/tests-filter.cpp
@@ -3,6 +3,7 @@
 #include <dicom-filter.h>
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 #include "common.h"
 
 extern OrthancPluginErrorCode WriteDicomFile(DicomFile dicom, const char *uuid);
@@ -23,28 +24,37 @@ TEST(filtering, store_filtered) {
     std::cout << "Configuring DicomFilter.." << std::endl;
     DicomFilter filter = DicomFilter::ParseConfig(config);
     std::cout << " configured" << std::endl;
+    size_t files_passed = 0;
+    size_t files_total = 0;
     auto test = [&](const fs::path &path){
+        char msg[1024] = {0};
+        files_total++;
         auto size = fs::file_size(path);
         std::unique_ptr<char[]> buffer(new char[size]);
-        std::cout << "Loading " << path << std::endl;
+
+        sprintf(msg,"Loading %s\n", path.c_str());
+        DEBUG_LOG(msg);
         std::ifstream file(path, std::ios::binary | std::ios::in);
         ASSERT_TRUE(file.is_open());
         file.read(buffer.get(),size);
         file.close();
         ASSERT_TRUE(file.good());
-        std::cout << "Parsing.." << std::endl;
         DicomFile dicom(buffer.get(), size); // valid
         ASSERT_TRUE(dicom.IsValid()); // pass
-        std::cout << "Loaded " << (dicom.IsValid() ? "valid" : "invalid") << " dicom file." << std::endl;
-        std::cout << "Filtering.." << std::endl;
+        sprintf(msg,"%s file\n",(dicom.IsValid() ? "valid" : "invalid"));
+        DEBUG_LOG(msg);
+
         auto [filtered_buffer,filtered_size] = filter.ApplyFilter(dicom); //memcpy ran til end of file(buffer)
         DicomFile filtered_dicom(filtered_buffer.get(),filtered_size);
         ASSERT_TRUE(filtered_dicom.IsValid()); //first sign the memcpy didn't work
-        std::cout << "Filtered data: " << (filtered_dicom.IsValid() ? "valid" : "invalid") << std::endl;
+        sprintf(msg,"Filtering: %s\n", (filtered_dicom.IsValid() ? "passed" : "failed"));
+        DEBUG_LOG(msg);
+
         filter.reset();
-        std::cout << "  FILE PASSED\n" << std::endl;
+        files_passed++;
     };
     //test(GetProjRoot().string() + "/samples/0002.DCM");
     TestWithDicomFiles(test);
+    std::cout << files_passed << "/" << files_total << " files passed" << std::endl;
     std::cout << "Test Complete." << std::endl;
 }
