fecd4693b4c2487f0ed8c4db2267ed527535d617
Josh Cooper <cooper.s.josh@outlook.com>
2022-02-18 09:57:35 -0800

    Adds one missing edge case
    There are more that were not added.

--- tests/test-datetruncation.cpp
+++ tests/test-datetruncation.cpp
@@ -5,23 +5,32 @@
 TEST(datetimetests, datetruncation){
     std::string defvalue = "19990323";
     const char* tcase0 = "YYYYMMDD";
-    const char* tcase1 = "YYYYMM10";
-    const char* tcase2 = "YYYY06DD";
-    const char* tcase3 = "2000MMDD";
+    const char* tcase1 = "YYYY0101";
+    const char* tcase2 = "YYYYMM10";
+    const char* tcase3 = "YYYY06DD";
+    const char* tcase4 = "2000MMDD";
 
     std::string value;
     value = TruncateDate(defvalue, tcase0);
     std::cout << value << std::endl;
     ASSERT_EQ(value, "19990323");
+
     value = TruncateDate(defvalue, tcase1);
     std::cout << value << std::endl;
-    ASSERT_EQ(value, "19990310");
+    ASSERT_EQ(value, "19990101");
+
     value = TruncateDate(defvalue, tcase2);
     std::cout << value << std::endl;
-    ASSERT_EQ(value, "19990623");
+    ASSERT_EQ(value, "19990310");
+
     value = TruncateDate(defvalue, tcase3);
     std::cout << value << std::endl;
+    ASSERT_EQ(value, "19990623");
+
+    value = TruncateDate(defvalue, tcase4);
+    std::cout << value << std::endl;
     ASSERT_EQ(value, "20000323");
+
     value = TruncateDate(defvalue);
     std::cout << value << std::endl;
     ASSERT_EQ(value, "19990323");
