4cc6d5c3ec2779501cae5434899065d2a833c191
Josh Cooper <cooper.s.josh@outlook.com>
2022-02-19 00:22:53 -0800

    Adds a few important dicom tag enums

--- include/dicom/dicom-tag.h
+++ include/dicom/dicom-tag.h
@@ -17,6 +17,9 @@ extern std::string KeyToHex(std::string key);
 
 // Dicom Tags/VR/Name found here https://www.dicomlibrary.com/dicom/dicom-tags/
 enum DicomTag{
+    DOB = 0x00300010,
+    PID = 0x00200010,
+    StudyDate = 0x00200008,
     Item = 0xe000fffe,
     ItemDelItem = 0xe00dfffe,
     SeqDelItem = 0xe0ddfffe
