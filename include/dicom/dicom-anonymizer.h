f76e62bbaf0ec82cae27abb4bae32ffb59e2bb11
Josh Cooper <cooper.s.josh@outlook.com>
2022-02-22 22:43:51 -0800

    Adds class comment block to dicom-anonymizer.h

--- include/dicom/dicom-anonymizer.h
+++ include/dicom/dicom-anonymizer.h
@@ -3,6 +3,18 @@
 #include <dicom-element-view.h>
 #include <dicom-tag.h>
 
+/* class: DicomAnonymizer
+ *  Used to anonymize DICOM files.
+ *
+ *  The user first acquires the DICOM file data then creates a DicomFile with the data buffer.
+ *  That DicomFile is then passed to the Anonymizer.
+ *
+ *  This could be used to make a standalone anonymization utility if someone were keen to. Wouldn't take much.
+ *
+ *  Provides methods to:
+ *   - Anonymize a DicomFile
+ *   - print the Anonymizer's configuration to stdout
+ */
 class DicomAnonymizer {
     friend class PluginConfigurer;
     friend class TestAnonymizer;
