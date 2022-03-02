823b6bd4b36f4a3c27e237c0db0512110c1b5822
Josh Cooper <cooper.s.josh@outlook.com>
2022-03-02 14:56:24 -0800

    Revises DBInterface to use member variable instead of global pointer

--- src/data/db-interface.cpp
+++ src/data/db-interface.cpp
@@ -2,7 +2,6 @@
 #include <db-interface.h>
 #include <pqxx/except.hxx>
 #include <iostream>
-pqxx::connection* con = nullptr;
 
 //https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 //postgresql://[userspec@][hostspec][/dbname][?paramspec]
@@ -17,38 +16,36 @@ pqxx::connection* con = nullptr;
 //  postgresql://other@localhost/otherdb?connect_timeout=10&application_name=myapp
 //  postgresql://host1:123,host2:456/somedb?target_session_attrs=any&application_name=myapp
 void DBInterface::Connect(std::string database, std::string host, std::string port, std::string username, std::string password) {
-    if(!con) {
-        char buffer[256];
-        sprintf(buffer, "postgresql://%s:%s@%s:%s/%s", username.c_str(), password.c_str(), host.c_str(), port.c_str(), database.c_str());
-        try {
-            static pqxx::connection c(buffer);
-            con = &c;
-//            con->prepare(
+    char buffer[256];
+    sprintf(buffer, "postgresql://%s:%s@%s:%s/%s", username.c_str(), password.c_str(), host.c_str(), port.c_str(), database.c_str());
+    try {
+        con = pqxx::connection(buffer);
+//            con.prepare(
 //                    "get_uuid_from_instanceid",
 //                    "SELECT a.uuid "
 //                    "FROM attachedfiles a, resources r "
 //                    "WHERE r.internalid = a.id AND r.publicid = $1;"
 //                    );
-            con->prepare(
-                    "UpdateChecksum",
-                    "UPDATE attachedfiles "
-                    "SET uncompressedsize = $1, compressedsize = $2, uncompressedhash = $3, compressedhash = $4 "
-                    "WHERE uuid = $5;"
-                    );
-        } catch (const std::exception &e){
-            std::cerr << e.what() << std::endl;
-        }
+        con.prepare(
+                "UpdateChecksum",
+                "UPDATE attachedfiles "
+                "SET uncompressedsize = $1, compressedsize = $2, uncompressedhash = $3, compressedhash = $4 "
+                "WHERE uuid = $5;"
+                );
+    } catch (const std::exception &e){
+        DEBUG_LOG(PLUGIN_ERRORS, "Failed to connect to database.")
+        std::cerr << e.what() << std::endl;
     }
 }
 
 void DBInterface::disconnect() {
-    if(con && con->is_open()){
-        con->close();
+    if(con.is_open()){
+        con.close();
     }
 }
 
 bool DBInterface::IsOpen() {
-    return con && con->is_open();
+    return con.is_open();
 }
 
 //std::string DBInterface::get_uuid_from_instanceid(const char* instanceid) {
@@ -62,9 +59,11 @@ bool DBInterface::IsOpen() {
 
 
 void DBInterface::UpdateChecksum(std::string uuid, int64_t size, const char* hash) {
-    pqxx::work w( *con);
-    w.exec_prepared("UpdateChecksum", size, size, hash, hash, uuid);
-    w.commit();
+    if(con.is_open()) {
+        pqxx::work w(con);
+        w.exec_prepared("UpdateChecksum", size, size, hash, hash, uuid);
+        w.commit();
+    }
     /*
     pqxx::result r = w.exec("SELECT * FROM attachedfiles;");
     DEBUG_LOG(1, "Current checksum in database: ");
@@ -82,32 +81,34 @@ void DBInterface::UpdateChecksum(std::string uuid, int64_t size, const char* has
 
 
 void DBInterface::CreateTables() {
-    pqxx::work w( *con);
-    w.exec0("CREATE SEQUENCE IF NOT EXISTS public.id_sequence\n"
-            "INCREMENT 1\n"
-            "START 1\n"
-            "MINVALUE 1;\n"
-            "CREATE TABLE IF NOT EXISTS public.dicom_instances (\n"
-            "    uuid VARCHAR PRIMARY KEY,\n"
-            "    timestamp int NOT NULL\n"
-            ");\n"
-            "\n"
-            "CREATE TABLE IF NOT EXISTS public.dicom_identifiers (\n"
-            "    id INT DEFAULT nextval('id_sequence'::regclass) NOT NULL,\n"
-            "    uuid VARCHAR NOT NULL,\n"
-            "    tag_group VARCHAR NOT NULL,\n"
-            "    tag_element VARCHAR NOT NULL,\n"
-            "    value VARCHAR,\n"
-            "    PRIMARY KEY (id),\n"
-            "    FOREIGN KEY (uuid) REFERENCES public.dicom_instances(uuid)\n"
-            ");\n"
-            "\n"
-            "CREATE TABLE IF NOT EXISTS public.dicom_duplicates (\n"
-            "    uuid VARCHAR PRIMARY KEY,\n"
-            "    parent_uuid VARCHAR,\n"
-            "    FOREIGN KEY (uuid) REFERENCES public.dicom_instances(uuid),\n"
-            "    FOREIGN KEY (parent_uuid) REFERENCES public.dicom_instances(uuid)\n"
-            ");\n"
-            );
-    w.commit();
+    if(con.is_open()) {
+        pqxx::work w(con);
+        w.exec0("CREATE SEQUENCE IF NOT EXISTS public.id_sequence\n"
+                "INCREMENT 1\n"
+                "START 1\n"
+                "MINVALUE 1;\n"
+                "CREATE TABLE IF NOT EXISTS public.dicom_instances (\n"
+                "    uuid VARCHAR PRIMARY KEY,\n"
+                "    timestamp int NOT NULL\n"
+                ");\n"
+                "\n"
+                "CREATE TABLE IF NOT EXISTS public.dicom_identifiers (\n"
+                "    id INT DEFAULT nextval('id_sequence'::regclass) NOT NULL,\n"
+                "    uuid VARCHAR NOT NULL,\n"
+                "    tag_group VARCHAR NOT NULL,\n"
+                "    tag_element VARCHAR NOT NULL,\n"
+                "    value VARCHAR,\n"
+                "    PRIMARY KEY (id),\n"
+                "    FOREIGN KEY (uuid) REFERENCES public.dicom_instances(uuid)\n"
+                ");\n"
+                "\n"
+                "CREATE TABLE IF NOT EXISTS public.dicom_duplicates (\n"
+                "    uuid VARCHAR PRIMARY KEY,\n"
+                "    parent_uuid VARCHAR,\n"
+                "    FOREIGN KEY (uuid) REFERENCES public.dicom_instances(uuid),\n"
+                "    FOREIGN KEY (parent_uuid) REFERENCES public.dicom_instances(uuid)\n"
+                ");\n"
+               );
+        w.commit();
+    }
 }
