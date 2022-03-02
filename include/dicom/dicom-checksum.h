066b054b207758a3035487a89c73826716baa697
Josh Cooper <cooper.s.josh@outlook.com>
2022-03-02 14:54:45 -0800

    Revises DicomChecksum in preparation for the incoming filter callback

--- include/dicom/dicom-checksum.h
+++ include/dicom/dicom-checksum.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <core.h>
-#include <dicom-file.h>
+
 #include <mutex>
+#include <unordered_map>
 
 using Uuid = std::string;
 using Md5 = std::string;
 
 class DicomChecksum {
 private:
-    static std::mutex map_lock;
-    static std::unordered_map<const void*, std::tuple<Uuid, Md5, size_t>> checksum_map;
+    static std::mutex checksum_lock;
+    static std::mutex uuid_lock;
+    static std::unordered_map<const void*, std::tuple<Md5, size_t>> checksum_map;
+    static std::unordered_map<const void*, Uuid> uuid_map;
 public:
-    static void SaveChecksum(const void* instance_data, std::string uuid, std::string md5, size_t size);
+    static void Emplace(const void* instance_data, std::string md5, size_t size);
+    static void Emplace(const void* instance_data, std::string uuid);
     static bool UpdateDatabase(const void* instance_data);
 };
--- src/dicom/dicom-checksum.cpp
+++ src/dicom/dicom-checksum.cpp
@@ -1,31 +1,43 @@
 #include <dicom-checksum.h>
 #include <db-interface.h>
-#include <string>
 
-std::unordered_map<const void *, std::tuple<std::string, std::string, size_t>> DicomChecksum::checksum_map;
-std::mutex DicomChecksum::map_lock;
+std::unordered_map<const void *, std::tuple<std::string, size_t>> DicomChecksum::checksum_map;
+std::unordered_map<const void *, std::string> DicomChecksum::uuid_map;
+std::mutex DicomChecksum::checksum_lock;
+std::mutex DicomChecksum::uuid_lock;
 
-void DicomChecksum::SaveChecksum(const void* instance_data, std::string uuid, std::string md5, size_t size) {
-    map_lock.lock();
-    checksum_map.emplace(instance_data, std::make_tuple(uuid, md5, size));
-    map_lock.unlock();
+void DicomChecksum::Emplace(const void* instance_data, std::string md5, size_t size) {
+    checksum_lock.lock();
+    checksum_map.emplace(instance_data, std::make_tuple(md5, size));
+    checksum_lock.unlock();
+}
+
+void DicomChecksum::Emplace(const void* instance_data, std::string uuid) {
+    uuid_lock.lock();
+    uuid_map.emplace(instance_data, uuid);
+    uuid_lock.unlock();
 }
 
 bool DicomChecksum::UpdateDatabase(const void* instance_data) {
     char msg[1024];
-    map_lock.lock();
-    auto iter = checksum_map.find(instance_data);
-    if (iter != checksum_map.end()) {
-        auto &[uuid, md5, size] = iter->second;
+    checksum_lock.lock();
+    auto checksum_iter = checksum_map.find(instance_data);
+    uuid_lock.lock();
+    auto uuid_iter = uuid_map.find(instance_data);
+    if (checksum_iter != checksum_map.end() && uuid_iter != uuid_map.end()) {
+        auto &[md5, size] = checksum_iter->second;
+        auto uuid = uuid_iter->second;
         sprintf(msg, "UUID: %s, MD5: %s", uuid.c_str(), md5.c_str());
-        DBInterface::UpdateChecksum(uuid, size, md5.c_str());
+        DBInterface::GetInstance().UpdateChecksum(uuid, size, md5.c_str());
         DEBUG_LOG(1, msg);
-        checksum_map.erase(iter);
-        map_lock.unlock();
+        checksum_map.erase(checksum_iter);
+        checksum_lock.unlock();
+        uuid_map.erase(uuid_iter);
+        uuid_lock.unlock();
         return true;
     }
     // Checksum not found in checksum_map
-    map_lock.unlock();
+    checksum_lock.unlock();
     sprintf(msg, "DicomChecksum::UpdateDatabase: Failure! No checksum was found for %zu.", instance_data);
     DEBUG_LOG(PLUGIN_ERRORS, msg);
     return false;
