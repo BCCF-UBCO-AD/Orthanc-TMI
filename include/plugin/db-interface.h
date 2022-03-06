a0a0926d89b9794c91978faec4ee1362ecfc8657
Josh Cooper <cooper.s.josh@outlook.com>
2022-03-06 00:46:52 -0800

    Guts DBInterface

--- include/plugin/db-interface.h
+++ include/plugin/db-interface.h
@@ -4,21 +4,9 @@
 // todo: name better?
 class DBInterface { // not an 'interface'
 private:
-    pqxx::connection* con = nullptr;
     DBInterface() = default;
-    void initialize();
-    void disconnect();
 public:
-    ~DBInterface() {
-        disconnect();
-    }
-    static DBInterface& GetInstance(){
-        static DBInterface instance;
-        return instance;
-    }
-    void Connect(std::string database, std::string host, uint16_t port, std::string username, std::string password);
-    bool IsOpen();
-    
     //static void UpdateChecksum(std::string uuid, int64_t size, char *hash);
-    void UpdateChecksum(std::string uuid, std::string hash, int64_t size);
+    static void UpdateChecksum(std::string uuid, std::string hash, int64_t size);
+    static void Initialize();
 };
