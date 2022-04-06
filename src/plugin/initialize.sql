5dbf49677338af5289f9da42d8de6477498c04f4
Brian Z <brianzhouzc@gmail.com>
2022-04-06 15:22:12 -0700

    New initialization script

--- src/plugin/initialize.sql
+++ src/plugin/initialize.sql
@@ -13,6 +13,15 @@ CREATE TABLE IF NOT EXISTS crosswalk (
     UNIQUE (internalid, publicid, patient_id, full_name, dob)
 );
 
+CREATE TABLE IF NOT EXISTS phi_mismatch(
+    id SERIAL NOT NULL,
+    internalid BIGINT NOT NULL UNIQUE,
+    publicid CHARACTER VARYING(64) NOT NULL UNIQUE,
+    parent_internalid BIGINT NOT NULL,
+    parent_publicid CHARACTER VARYING(64) NOT NULL,
+    PRIMARY KEY (id),
+);
+
 CREATE INDEX IF NOT EXISTS i_internalid ON crosswalk (internalid);
 CREATE INDEX IF NOT EXISTS i_publicid ON crosswalk (publicid);
 CREATE INDEX IF NOT EXISTS i_lower_full_name ON crosswalk (lower(full_name));
@@ -59,18 +68,6 @@ BEGIN
     END;
 $insert_info_crosswalk$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS phi_mismatch(
-	internalid BIGINT NOT NULL UNIQUE,
-	publicid CHARACTER VARYING(64) NOT NULL UNIQUE,
-	parent_internalid BIGINT NOT NULL,
-	parent_publicid CHARACTER VARYING(64) NOT NULL,
-	PRIMARY KEY (internalid),
-	FOREIGN KEY (internalid) REFERENCES resources(internalid) ON DELETE CASCADE,
-	FOREIGN KEY (publicid) REFERENCES resources(publicid) ON DELETE CASCADE,
-	FOREIGN KEY (parent_internalid) REFERENCES resources(internalid) ON DELETE CASCADE,
-	FOREIGN KEY (parent_publicid) REFERENCES resources(publicid) ON DELETE CASCADE
-);
-
 CREATE OR REPLACE FUNCTION detect_phi_mismatch() RETURNS trigger AS $detect_phi_mismatch$
 	DECLARE
 		dob_fullname_internalid BIGINT;
@@ -78,23 +75,43 @@ CREATE OR REPLACE FUNCTION detect_phi_mismatch() RETURNS trigger AS $detect_phi_
 		dob_lastname_internalid BIGINT;
 		dob_lastname_publicid TEXT;
 	BEGIN
-		/* Wait until all info is in crosswalk */
-		IF NEW.internalid IS NOT NULL AND NEW.patient_id IS NOT NULL AND NEW.full_name IS NOT NULL AND NEW.dob IS NOT NULL THEN
-			SELECT internalid, publicid INTO dob_fullname_internalid, dob_fullname_publicid FROM crosswalk WHERE internalid <> NEW.internalid AND dob = NEW.dob AND lower(full_name) = lower(NEW.full_name) ORDER BY internalid ASC LIMIT 1;
-			IF dob_fullname_internalid IS NOT NULL THEN
-				INSERT INTO phi_mismatch VALUES(NEW.internalid, NEW.publicid, dob_fullname_internalid, dob_fullname_publicid);
-				RAISE NOTICE 'PHI mismatch detected - DOB & Full Name matched: parent_internalid: %, parent_publicid: %', dob_fullname_internalid, dob_fullname_publicid;
-			ELSE
-				SELECT internalid, publicid INTO dob_lastname_internalid, dob_lastname_publicid FROM crosswalk WHERE internalid <> NEW.internalid AND dob = NEW.dob AND lower(last_name) = lower(NEW.last_name) ORDER BY internalid ASC LIMIT 1;
-				IF dob_lastname_internalid IS NOT NULL THEN
-					INSERT INTO phi_mismatch VALUES(NEW.internalid, NEW.publicid, dob_lastname_internalid, dob_lastname_publicid);
-					RAISE NOTICE 'PHI mismatch detected - DOB & Last Name matched: parent_internalid: %, parent_publicid: %', dob_lastname_internalid, dob_lastname_publicid;
-				END IF;
-			END IF;			
-		END IF;
+        SELECT internalid, publicid FROM crosswalk
+        INTO
+            dob_fullname_internalid, dob_fullname_publicid
+        WHERE
+            internalid <> NEW.internalid AND
+            dob = NEW.dob AND
+            lower(full_name) = lower(NEW.full_name)
+        ORDER BY internalid ASC
+        LIMIT 1;
+
+        /* Detect case 5 and 6 */
+        IF dob_fullname_internalid IS NOT NULL THEN
+            INSERT INTO phi_mismatch VALUES(NEW.internalid, NEW.publicid, dob_fullname_internalid, dob_fullname_publicid);
+            RAISE NOTICE 'PHI mismatch detected - DOB & Full Name matched: parent_internalid: %, parent_publicid: %', dob_fullname_internalid, dob_fullname_publicid;
+        ELSE
+            SELECT internalid, publicid
+            INTO
+                dob_lastname_internalid, dob_lastname_publicid
+            FROM crosswalk
+            WHERE
+                  internalid <> NEW.internalid AND
+                  dob = NEW.dob AND
+                  lower(last_name) = lower(NEW.last_name)
+            ORDER BY internalid ASC
+            LIMIT 1;
+
+            IF dob_lastname_internalid IS NOT NULL THEN
+                INSERT INTO phi_mismatch VALUES(NEW.internalid, NEW.publicid, dob_lastname_internalid, dob_lastname_publicid);
+                RAISE NOTICE 'PHI mismatch detected - DOB & Last Name matched: parent_internalid: %, parent_publicid: %', dob_lastname_internalid, dob_lastname_publicid;
+            END IF;
+        END IF;
+
+
 		RETURN NULL;
 	END;
 $detect_phi_mismatch$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE TRIGGER new_patient AFTER UPDATE
     ON crosswalk
     FOR EACH ROW
